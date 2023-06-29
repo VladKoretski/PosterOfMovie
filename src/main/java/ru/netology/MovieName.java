5ab1638ce2b1d129d5a4eb631b2b9da14c55e8e0
Vladko1973 <vpk1973@yandex.ru>
2023-06-29 12:27:26 +0300

    second commit

--- src/main/java/ru/netology/MovieName.java
+++ src/main/java/ru/netology/MovieName.java
@@ -1,2 +1,21 @@
-package ru.netology;public class MovieName {
+package ru.netology;
+
+public class MovieName {
+    String nameOfMovie;
+
+    public MovieName(String nameOfMovie) {
+        this.nameOfMovie = nameOfMovie;
+    }
+
+    public MovieName() {
+    }
+
+    public String getNameOfMovie() {
+        return nameOfMovie;
+    }
+
+    public void setNameOfMovie(String nameOfMovie) {
+        this.nameOfMovie = nameOfMovie;
+    }
 }
+
--- src/main/java/ru/netology/MoviePosterManager.java
+++ src/main/java/ru/netology/MoviePosterManager.java
@@ -1,2 +1,58 @@
-package ru.netology;public class MoviePosterManager {
+package ru.netology;
+
+public class MoviePosterManager {
+
+    public MovieName[] posters = new MovieName[0];
+    int resultLength = 5;
+
+    // Constructors
+    public MoviePosterManager() {
+    }
+
+    public MoviePosterManager(MovieName[] posters) {
+
+        this.posters = posters;
+    }
+
+    public MoviePosterManager(int resultLength) {
+
+        this.resultLength = resultLength;
+    }
+
+    public MoviePosterManager(MovieName[] posters, int resultLength) {
+        this.posters = posters;
+        this.resultLength = resultLength;
+    }
+
+    // File adding
+    public void addNewMovie(MovieName nameOfMovie) {
+
+        MovieName[] tempPosters = new MovieName[posters.length + 1];
+        System.arraycopy(posters, 0, tempPosters, 0, posters.length);
+        tempPosters[posters.length] = nameOfMovie;
+        posters = tempPosters;
+    }
+
+    //Manager's arts
+    public MovieName[] getPosters() {
+        return posters;
+    }
+
+    public MovieName[] findAll() {
+        return posters;
+    }
+
+    public MovieName[] findLast() {
+
+        int actualLength = resultLength;
+        if (actualLength > posters.length) {
+            actualLength = posters.length;
+        }
+        MovieName[] lastMovies = new MovieName[actualLength];
+
+        for (int i = 0; i < actualLength; i++) {
+            lastMovies[i] = posters[posters.length - 1 - i];
+        }
+        return lastMovies;
+    }
 }
